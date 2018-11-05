f78b9ae82df3337ff7a81736f900198c2eede05f
Jimmy <jabez007@users.noreply.github.com>
2018-11-05 10:47:21 -0600

    Update StreamClient.cs

--- IBMWatson.SpeechToText.Recognize.WebSocket/StreamClient.cs
+++ IBMWatson.SpeechToText.Recognize.WebSocket/StreamClient.cs
@@ -127,7 +127,7 @@ namespace IBMWatson.SpeechToText.Recognize.WebSocket
 
   internal class TranscriptionEventArgs : EventArgs
   {
-    public readonly SpeechRecognitionResults Response;
+    public readonly RecognitionResponse Response;
 
     public TranscriptionEventArgs(string results)
     {
